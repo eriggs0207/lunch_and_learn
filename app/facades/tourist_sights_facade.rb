f1e7aa2a4cf91830d6939ef475d1acecd091fcbd
Erik Riggs <riggs.erik@gmail.com>
2022-11-14 14:52:24 -0700

    feat/test: add tourist_site_facade and poro

--- app/facades/tourist_sights_facade.rb
+++ app/facades/tourist_sights_facade.rb
@@ -1,11 +1,12 @@
-class RecipeFacade
+class TouristSightsFacade
 
   def self.find_attractions(search_term)
     country = CountryService.one_country(search_term)
-    sight_data = TouristSightsService.find_attractions(country[1], country[0])
-    sight[:data]
-    recipe_data[:hits].map do |data|
-      Recipe.new(data[:recipe], country)
+    sight_data = TouristSightsService.attraction_by_lnglat(country[1], country[0])
+
+    sight_data[:features].map do |data|
+      x = TouristSight.new(data)
+      binding.pry
     end
   end
 end
--- spec/facades/tourist_sights_facade_spec.rb
+++ spec/facades/tourist_sights_facade_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe 'Tourist Sights Facade', :vcr do
   it 'find_attractions' do
-    country = CountryService.one("France")
-    sights = TouristSightsService.find_attractions(country[1], country[0])
+    # country = CountryService.one_country("Latvia")
+    # TouristSightsService.attraction_by_lnglat(country[1], country[0])
+    sights = TouristSightsFacade.find_attractions("Latvia")
 
     expect(sights).to be_an(Array)
-    expect(sights[0]).to be_a(Recipe)
-    expect(sights[0].country).to eq("Thailand")
-    expect(country).to be_an(Array)
+    # expect(sights[0]).to be_a(Recipe)
+    # expect(sights[0].country).to eq("Thailand")
+    # expect(country).to be_an(Array)
   end
 end
