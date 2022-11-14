ad91d0d050b060472f31c4c494e11d89922902c5
Erik Riggs <riggs.erik@gmail.com>
2022-11-13 19:51:38 -0700

    feat/test: add create function for favorites with happy path testing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,6 +16,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :email)
+    params.require(:user).permit(:name, :email)
   end
 end
