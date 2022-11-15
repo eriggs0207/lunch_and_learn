fcf4576f38436c8889bfae99fa967ef09055d7c4
Erik Riggs <riggs.erik@gmail.com>
2022-11-15 09:21:39 -0700

    test: add sad path test to create favorite

--- spec/facades/recipe_facade_spec.rb
+++ spec/facades/recipe_facade_spec.rb
@@ -4,7 +4,7 @@ describe 'Recipe Facade', :vcr do
   it 'recipes_search' do
     random = CountryService.random_country
     recipe = RecipeFacade.recipe_search("Thailand")
-
+    
     expect(recipe).to be_an(Array)
     expect(recipe[0]).to be_a(Recipe)
     expect(recipe[0].country).to eq("Thailand")
--- spec/requests/api/v1/favorites/create_spec.rb
+++ spec/requests/api/v1/favorites/create_spec.rb
@@ -22,7 +22,35 @@ RSpec.describe 'Favorites API | Create' do
         post '/api/v1/favorites', headers: headers, params: JSON.generate(fav_params)
         parsed_response = JSON.parse(response.body, symbolize_names: true)
         expect(response).to be_successful
+
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response[:success][:message]).to eq("Favorite added successfully")
+
+        expect(Favorite.all.count).to eq(1)
       end
     end
+
+    context 'sad path' do
+      it 'returns a 401 error when api_key is not present' do
+
+        fav_params = {
+          api_key: "",
+          country: @country,
+          recipe_link: @recipe.url,
+          recipe_title: @recipe.title
+
+        }
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post '/api/v1/favorites', headers: headers, params: JSON.generate(fav_params)
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to have_http_status(401)
+
+
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response[:errors][:message]).to eq("api key not vaild")
+
+      end
+    end 
   end
 end
