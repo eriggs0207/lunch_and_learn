05ac9005a68b735e425133c67fe02bec163fec9f
Erik Riggs <riggs.erik@gmail.com>
2022-11-15 13:43:25 -0700

    test: added extra test to recipe facade

--- spec/facades/recipe_facade_spec.rb
+++ spec/facades/recipe_facade_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 describe 'Recipe Facade', :vcr do
-  it 'recipes_search' do
+  it 'random_search' do
+    allow(CountryService).to receive(:random_country).and_return("Thailand")
     random = CountryService.random_country
-    recipe = RecipeFacade.recipe_search("Thailand")
-    
+    recipe = RecipeFacade.recipe_search("random_country")
+
     expect(recipe).to be_an(Array)
     expect(recipe[0]).to be_a(Recipe)
-    expect(recipe[0].country).to eq("Thailand")
+    expect(recipe[0].country).to eq(random)
     expect(random).to be_a(String)
+  end
+
+  it 'country_search' do
+    recipe = RecipeFacade.recipe_search("Belgium")
+
+    expect(recipe).to be_an(Array)
+    expect(recipe[0]).to be_a(Recipe)
+    expect(recipe[0].country).to eq("Belgium")
   end
 end
